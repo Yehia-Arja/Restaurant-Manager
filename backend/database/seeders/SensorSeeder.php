549aafc0cb695c9c75002f135f7c7c9924e9447a
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-23 23:20:25 +0300

    added sensor seeder

--- backend/database/seeders/SensorSeeder.php
+++ backend/database/seeders/SensorSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Sensor;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class SensorSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Sensor::factory()->count(50)->create();
     }
 }
