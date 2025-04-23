7679d8abeba99f220a2bb238ae4509d65186464b
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-23 22:46:06 +0300

    added restaurant factory

--- backend/database/factories/RestaurantFactory.php
+++ backend/database/factories/RestaurantFactory.php
@@ -17,7 +17,8 @@ class RestaurantFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->company(),
+            'description' => $this->faker->optional()->paragraph(),
         ];
     }
 }
