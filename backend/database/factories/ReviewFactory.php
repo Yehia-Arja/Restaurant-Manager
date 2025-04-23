ce48f380bbc6f3c48950c1f5aa75f19714bdc9cf
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-23 22:47:44 +0300

    added review factory

--- backend/database/factories/ReviewFactory.php
+++ backend/database/factories/ReviewFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Order;
+use App\Models\RestaurantLocation;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Review>
@@ -16,8 +18,22 @@ class ReviewFactory extends Factory
      */
     public function definition(): array
     {
+        static $orderIds = null;
+        static $locationIds = null;
+
+        if (is_null($orderIds)) {
+            $orderIds = Order::pluck('id')->toArray();
+        }
+
+        if (is_null($locationIds)) {
+            $locationIds = RestaurantLocation::pluck('id')->toArray();
+        }
+
         return [
-            //
+            'rating' => $this->faker->numberBetween(1, 5),
+            'comment' => $this->faker->optional()->sentence(),
+            'order_id' => $orderIds[array_rand($orderIds)],
+            'restaurant_location_id' => $locationIds[array_rand($locationIds)],
         ];
     }
 }
