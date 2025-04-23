252eee28de7c3a2990f53967f2bb4e20e85965dc
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-23 22:43:59 +0300

    added product factory

--- backend/database/factories/ProductLocationFactory.php
+++ backend/database/factories/ProductLocationFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Product;
+use App\Models\RestaurantLocation;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ProductLocation>
@@ -16,8 +18,22 @@ class ProductLocationFactory extends Factory
      */
     public function definition(): array
     {
+        static $productIds = null;
+        static $locationIds = null;
+
+        if (is_null($productIds)) {
+            $productIds = Product::pluck('id')->toArray();
+        }
+
+        if (is_null($locationIds)) {
+            $locationIds = RestaurantLocation::pluck('id')->toArray();
+        }
+
         return [
-            //
+            'product_id' => $productIds[array_rand($productIds)],
+            'restaurant_location_id' => $locationIds[array_rand($locationIds)],
+            'override_price' => $this->faker->optional()->randomFloat(2, 5, 40),
+            'override_description' => $this->faker->optional()->sentence(),
         ];
     }
 }
