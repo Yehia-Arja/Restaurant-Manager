6bc56f89afefea48f141f0b103ed7738d7beaa5e
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-23 23:22:40 +0300

    added user_preference seeder

--- backend/database/seeders/UserPreferenceSeeder.php
+++ backend/database/seeders/UserPreferenceSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\UserPreference;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class UserPreferenceSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        UserPreference::factory()->count(50)->create();
     }
 }
