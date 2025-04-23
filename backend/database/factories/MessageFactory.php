86f48d6a9e75012235b9c5b8c17fee92a6349f05
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-23 22:41:32 +0300

    added message factory

--- backend/database/factories/MessageFactory.php
+++ backend/database/factories/MessageFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Chat;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Message>
@@ -16,8 +18,22 @@ class MessageFactory extends Factory
      */
     public function definition(): array
     {
+        static $userIds = null;
+        static $chatIds = null;
+
+        if (is_null($userIds)) {
+            $userIds = User::pluck('id')->toArray();
+        }
+
+        if (is_null($chatIds)) {
+            $chatIds = Chat::pluck('id')->toArray();
+        }
+
         return [
-            //
+            'sender_type' => $this->faker->randomElement(['user', 'ai']),
+            'content' => $this->faker->sentence(),
+            'user_id' => $userIds[array_rand($userIds)],
+            'chat_id' => $chatIds[array_rand($chatIds)],
         ];
     }
 }
