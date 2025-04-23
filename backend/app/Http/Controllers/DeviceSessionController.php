f3372aaf7ed7dc35bf06d7160ce8bd80249ea5b2
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-23 18:03:34 +0300

    added the database environment

---
+++ backend/app/Http/Controllers/DeviceSessionController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DeviceSession;
+use Illuminate\Http\Request;
+
+class DeviceSessionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DeviceSession $deviceSession)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DeviceSession $deviceSession)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, DeviceSession $deviceSession)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DeviceSession $deviceSession)
+    {
+        //
+    }
+}
