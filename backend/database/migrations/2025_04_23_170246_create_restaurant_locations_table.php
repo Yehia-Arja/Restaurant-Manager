80c257ed88a2bd3f05df024d3bef651605630764
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-23 20:06:12 +0300

    added restaurant_locations migration

--- backend/database/migrations/2025_04_23_170246_create_restaurant_locations_table.php
+++ backend/database/migrations/2025_04_23_170246_create_restaurant_locations_table.php
@@ -13,7 +13,14 @@ return new class extends Migration
     {
         Schema::create('restaurant_locations', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+			$table->string('location_name');
+			$table->string('address');
+			$table->string('city');
+			$table->string('floor_plan')->nullable(); // image
+			$table->timestamps();
+
+            // Foreign key
+            $table->foreignId('restaurant_id')->constrained('restaurants')->onDelete('cascade');
         });
     }
 
