b673eef0a5ab5ffa6fe8a95ef5a192c1651f5e29
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-23 22:28:04 +0300

    edited the categories to support multiple branches

---
+++ backend/database/seeders/CategoryLocationSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategoryLocationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
