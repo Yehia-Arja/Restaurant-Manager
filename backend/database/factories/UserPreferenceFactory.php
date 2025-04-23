e0627afc9aeb6b259a9f0371f926f995f48d36ec
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-23 22:58:07 +0300

    finished the factories

--- backend/database/factories/UserPreferenceFactory.php
+++ backend/database/factories/UserPreferenceFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\UserPreference>
@@ -16,8 +17,19 @@ class UserPreferenceFactory extends Factory
      */
     public function definition(): array
     {
+        static $userIds = null;
+
+        if (is_null($userIds)) {
+            $userIds = User::pluck('id')->toArray();
+        }
+
         return [
-            //
+            'user_id' => $userIds[array_rand($userIds)],
+            'summary' => json_encode([
+                'likes' => $this->faker->randomElements(['spicy', 'cheesy', 'grilled', 'sweet'], rand(1, 2)),
+                'dislikes' => $this->faker->randomElements(['salty', 'sour', 'bitter'], rand(0, 2)),
+                'favorite_dishes' => $this->faker->randomElements(range(1, 100), rand(0, 3))
+            ])
         ];
     }
 }
