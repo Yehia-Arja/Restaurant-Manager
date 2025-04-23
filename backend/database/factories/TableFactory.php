9369a3e675099ad066e1ee37a22077990693b0dc
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-23 22:49:13 +0300

    added table factory

--- backend/database/factories/TableFactory.php
+++ backend/database/factories/TableFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\RestaurantLocation;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Table>
@@ -16,8 +17,19 @@ class TableFactory extends Factory
      */
     public function definition(): array
     {
+        static $locationIds = null;
+
+        if (is_null($locationIds)) {
+            $locationIds = RestaurantLocation::pluck('id')->toArray();
+        }
+
         return [
-            //
+            'position' => json_encode([
+                'x' => $this->faker->numberBetween(0, 1000),
+                'y' => $this->faker->numberBetween(0, 1000),
+            ]),
+            'floor' => $this->faker->numberBetween(0, 3),
+            'restaurant_location_id' => $locationIds[array_rand($locationIds)],
         ];
     }
 }
