f3af035e089557ebee68d0a588d73355e3f2352b
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-23 23:12:18 +0300

    added chair seeder

--- backend/database/seeders/ChairSeeder.php
+++ backend/database/seeders/ChairSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Chair;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class ChairSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Chair::factory()->count(60)->create();
     }
 }
