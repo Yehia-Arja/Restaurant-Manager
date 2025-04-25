7d66983d699fbe5704380899819c2424a50f258c
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-25 23:46:19 +0300

    added the login controller

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use App\Services\LoginService;
+use Illuminate\Container\Attributes\Log;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    
+    public function login(LoginRequest $request) {
+        $credentials = $request->validated();
+        $data = LoginService::login($credentials);
+        
+        if (!$data) {
+            return $this->error('Invalid credentials', 401);
+        }
+        return $this->success($data, 'Login successful');
+    }
 }
