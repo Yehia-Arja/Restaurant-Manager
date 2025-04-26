c80da6956eaf51b0114491e67c7a57b149fbd16e
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-26 12:44:29 +0300

    added the general colors file

---
+++ frontend/mobile/lib/core/theme/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+    static const color primary = Color(0xFAF9F6);
+    static const secondary = Color(0x1F1F1F);
+    static const accent = Color(0xFF8127);
+    static const label = Color(0x7C7C7C);
+    static const placeholder = Color(0xA1A1A1);
+    static const border = Color(0xECECEC);
+}
