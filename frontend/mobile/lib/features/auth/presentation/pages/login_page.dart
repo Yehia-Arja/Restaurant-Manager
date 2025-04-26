3cf542231b0e25ea66d14905ef610c03fcb3e22a
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-26 21:44:12 +0300

    started with the login page

---
+++ frontend/mobile/lib/features/auth/presentation/pages/login_page.dart
@@ -0,0 +1,14 @@
+class LoginPage extends StatelessWidget {
+    const LoginPage({super.key});
+
+    @override
+    Widget build(BuildContext context){
+        return Scaffold(
+            body: SafeArea(
+                child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                )
+            )
+        )
+    }
+}
