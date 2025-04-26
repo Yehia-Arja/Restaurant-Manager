2b00bb74604e6df159790bb93a756682022540c9
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-26 12:48:21 +0300

    fixed the colors file added the first ff (opacity)

--- frontend/mobile/lib/core/theme/colors.dart
+++ frontend/mobile/lib/core/theme/colors.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-    static const color primary = Color(0xFAF9F6);
-    static const secondary = Color(0x1F1F1F);
-    static const accent = Color(0xFF8127);
-    static const label = Color(0x7C7C7C);
-    static const placeholder = Color(0xA1A1A1);
-    static const border = Color(0xECECEC);
+    static const Color primary = Color(0xFAF9F6);
+    static const Color secondary = Color(0x1F1F1F);
+    static const Color accent = Color(0xFF8127);
+    static const Color label = Color(0x7C7C7C);
+    static const Color placeholder = Color(0xA1A1A1);
+    static const Color border = Color(0xECECEC);
 }
