802f1d5d2b3da0097c253f061d1f81a97c302882
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-28 02:47:11 +0300

    adjusted the baseUrl so it can work on the emulator

--- frontend/mobile/lib/core/services/dio_service.dart
+++ frontend/mobile/lib/core/services/dio_service.dart
@@ -3,7 +3,7 @@ import 'package:dio/dio.dart';
 class DioService {
     static final Dio dio = Dio(
         BaseOptions(
-            baseUrl: 'http://127.0.0.1:8000/api/v0.1/',
+            baseUrl: 'http://10.0.2.2:8000/api/v0.1/',
             connectTimeout: const Duration(seconds: 10),
             receiveTimeout: const Duration(seconds: 10),
             headers: {
