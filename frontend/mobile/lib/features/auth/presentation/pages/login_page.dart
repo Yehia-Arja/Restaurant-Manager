1cda335091f1536ce6cdfb2990697a3e3e009904
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-28 00:21:40 +0300

    added the icons in login page and finished the ui

--- frontend/mobile/lib/features/auth/presentation/pages/login_page.dart
+++ frontend/mobile/lib/features/auth/presentation/pages/login_page.dart
@@ -1,14 +1,132 @@
-class LoginPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:mobile/core/theme/colors.dart';
+import 'package:mobile/core/theme/theme.dart';
+import 'package:mobile/shared/widgets/base_scaffold.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:mobile/shared/widgets/custom_button.dart';
+
+class LoginPage extends StatefulWidget {
     const LoginPage({super.key});
 
     @override
-    Widget build(BuildContext context){
-        return Scaffold(
-            body: SafeArea(
-                child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                )
-            )
-        )
+    State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+    bool _isPasswordVisible = false;
+
+    @override
+    Widget build(BuildContext context) {
+        return BaseScaffold(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                    Align(
+                        alignment: Alignment.topLeft,
+                        child: IconButton(
+                            onPressed: () => Navigator.pop(context),
+                            icon: const Icon(
+                                Icons.chevron_left, 
+                                size: 24, 
+                                color: AppColors.placeholder,
+                            ),
+                        ),
+                    ),
+
+                    const SizedBox(height: 16),
+
+                    Text(
+                        'Login',
+                        style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                            color: AppColors.secondary,
+                        ),
+                    ),
+
+                    const SizedBox(height: 16),
+
+                    Text(
+                        'Login to your account',
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                            color: AppColors.label,
+                        ),
+                    ),
+
+                    const SizedBox(height: 16),
+
+                    Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                            'Email',
+                            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                                color: AppColors.label,
+                            ),
+                        ),
+                    ),
+
+                    const SizedBox(height: 4),
+
+                    TextField(
+                        decoration: InputDecoration(
+                            hintText: 'Enter your email',
+                            prefixIcon: const Icon(Icons.email, color: AppColors.placeholder),
+                        ),
+                    ),
+
+                    const SizedBox(height: 16),
+
+                    Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                            'Password',
+                            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                                color: AppColors.label,
+                            ),
+                        ),
+                    ),
+
+                    const SizedBox(height: 4),
+
+                    TextField(
+                        obscureText: !_isPasswordVisible,
+                        decoration: InputDecoration(
+                            hintText: 'Enter your password',
+                            prefixIcon: const Icon(Icons.lock, color: AppColors.placeholder),
+                            suffixIcon: Padding(
+                                padding: const EdgeInsets.only(right: 16), // 16px from right
+                                child: IconButton(
+                                    icon: Icon(
+                                        _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                                        color: AppColors.placeholder,
+                                    ),
+                                    onPressed: () {
+                                        setState(() {
+                                            _isPasswordVisible = !_isPasswordVisible;
+                                        });
+                                    },
+                                ),
+                            ),
+                        ),
+                    ),
+
+                    const SizedBox(height: 16),
+
+                    CustomButton(
+                        text: 'Login',
+                        onPressed: () {
+                            // Handle login action
+                        },
+                    ),
+
+                    const SizedBox(height: 16),
+
+                    SvgPicture.asset(
+                        'lib/assets/images/login.svg',
+                        width: MediaQuery.of(context).size.width * 0.9, // 90% of screen width
+                        height: MediaQuery.of(context).size.height * 0.4, // 40% of screen height
+                        fit: BoxFit.contain,
+                    ),
+                ],
+            ),
+        );
     }
 }
