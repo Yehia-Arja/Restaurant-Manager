3cfb04112771092f36eb052b041df83bf95ee7e8
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-28 02:16:22 +0300

    added the login api calling file

---
+++ frontend/mobile/lib/features/auth/data/auth_api.dart
@@ -0,0 +1,38 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthAPI {
+    static Future<void> login({
+        required String email,
+        required String password,
+    }) async {
+        try {
+            final response = await DioService.dio.post(
+                '/login',
+                data: {
+                    'email': email,
+                    'password': password,
+                },
+            );
+
+            if (response.statusCode != 200) {
+                throw Exception('Failed to login: ${response.statusCode}');
+            }
+
+            if (response.data == null) {
+                throw Exception('No data received from server');
+            }
+
+            final data = response.data;
+            final token = data['access_token'];
+
+            final prefs = await SharedPreferences.getInstance();
+            await prefs.setString('token', token);
+
+            return;
+
+        } catch (e) {
+            print('Login error: $e');
+            rethrow;
+        }
+    }
+}
