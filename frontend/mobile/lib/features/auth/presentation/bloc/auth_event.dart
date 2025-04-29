1e2fe3ea5a8c09504d2133a889f49e5ae9838d13
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-30 01:43:06 +0300

    added the signup event and by that finished the signup

--- frontend/mobile/lib/features/auth/presentation/bloc/auth_event.dart
+++ frontend/mobile/lib/features/auth/presentation/bloc/auth_event.dart
@@ -4,3 +4,8 @@ class LoginRequested extends AuthEvent {
   final String email, password;
   LoginRequested(this.email, this.password);
 }
+
+class SignupRequested extends AuthEvent {
+  final String firstName, lastName, email, password, confirmPassword;
+  SignupRequested(this.firstName, this.lastName, this.email, this.password, this.confirmPassword);
+}
