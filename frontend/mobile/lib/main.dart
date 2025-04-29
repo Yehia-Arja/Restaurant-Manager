83a9f6fdb6ae96ef1e40cad758e88c6fb948b5b6
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-29 16:28:48 +0300

    added my theme to the main plus added the onboarding route

--- frontend/mobile/lib/main.dart
+++ frontend/mobile/lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mobile/core/theme/theme.dart';
 import 'package:mobile/features/auth/presentation/pages/login_page.dart';
+import 'package:mobile/features/auth/presentation/pages/onboarding_page.dart';
 
 void main() {
     WidgetsFlutterBinding.ensureInitialized();
     runApp(
         GetMaterialApp(
+            theme: AppTheme.lightTheme,
             debugShowCheckedModeBanner: false,
-            initialRoute: '/login',
+            initialRoute: '/',
             getPages: [
+                GetPage(name: '/', page: () => const OnboardingPage()),
                 GetPage(name: '/login', page: () => const LoginPage()),
             ],
         ),
