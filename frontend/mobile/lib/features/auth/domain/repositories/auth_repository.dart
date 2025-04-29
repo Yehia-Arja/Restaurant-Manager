730c9103bf074a74299d5e02b7993cb3743e93e2
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-30 01:27:36 +0300

    added the method signup

--- frontend/mobile/lib/features/auth/domain/repositories/auth_repository.dart
+++ frontend/mobile/lib/features/auth/domain/repositories/auth_repository.dart
@@ -2,4 +2,11 @@ import 'package:mobile/features/auth/domain/entities/user.dart';
 
 abstract class AuthRepository {
   Future<User> login({required String email, required String password});
+  Future<User> signup({
+    required String firstName,
+    required String lastName,
+    required String email,
+    required String password,
+    required String confirmPassword,
+  });
 }
