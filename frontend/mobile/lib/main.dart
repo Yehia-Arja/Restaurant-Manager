441ad87704408dca940f8a38e9bff443ee2a7d52
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-30 01:44:30 +0300

    added the usecase in the main file

--- frontend/mobile/lib/main.dart
+++ frontend/mobile/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mobile/core/services/dio_service.dart';
 import 'package:mobile/core/theme/theme.dart';
+import 'package:mobile/features/auth/domain/usecases/signup_usecase.dart';
 import 'package:mobile/features/auth/presentation/screens/signup_screen.dart';
 import 'package:mobile/features/auth/data/datasources/auth_remote.dart';
 import 'package:mobile/features/auth/data/repositories/auth_repository_impl.dart';
@@ -17,12 +18,11 @@ void main() {
   final authRemote = AuthRemote(DioService.dio);
   final authRepo = AuthRepositoryImpl(authRemote);
   final loginUseCase = LoginUseCase(authRepo);
+  final signupUseCase = SignupUseCase(authRepo);
 
   runApp(
     MultiBlocProvider(
-      providers: [
-        BlocProvider<AuthBloc>(create: (_) => AuthBloc(loginUseCase)),
-      ],
+      providers: [BlocProvider<AuthBloc>(create: (_) => AuthBloc(loginUseCase, signupUseCase))],
       child: const MyApp(),
     ),
   );
