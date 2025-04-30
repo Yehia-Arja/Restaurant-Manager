e84a268698b4b9ffe7079eef3d86f84bddd98afa
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-30 13:53:19 +0300

    added the login testing

--- backend/app/Services/AuthService.php
+++ backend/app/Services/AuthService.php
@@ -21,7 +21,15 @@ class AuthService
         return [
             'user' => [
                 'id' => $user->id,
-                'name' => $user->name,
+                'first_name' => $user->first_name,
+                'last_name' => $user->last_name,
+                'user_type_id' => $user->user_type_id,
+                'restaurant_id' => $user->restaurant_id,
+                'restaurant_location_id' => $user->restaurant_location_id,
+                'phone_number' => $user->phone_number,
+                'date_of_birth' => $user->date_of_birth,
+                'google_id' => $user->google_id,
+                'provider' => $user->provider,
                 'email' => $user->email,
             ],
             'access_token' => $token,
