11f4e28fcf1b301116c0ac1d8be9469a87bbc420
Yehia Arja <yehia.al.arja12@gmail.com>
2025-05-02 17:09:09 +0300

    fixed the response returned only the neccessary data

--- smartDine-server/app/Http/Controllers/Common/AuthController.php
+++ smartDine-server/app/Http/Controllers/Common/AuthController.php
@@ -1,6 +1,7 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Common;
+use App\Http\Controllers\Controller;
 
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\SignupRequest;
