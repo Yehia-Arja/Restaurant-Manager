24fb362ba911be6df682d9f5812a56b0ed2ec09d
Yehia Arja <yehia.al.arja12@gmail.com>
2025-05-03 18:35:44 +0300

    added the fetching category request

---
+++ smartDine-server/app/Http/Requests/Common/CategoryRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\Common;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoryRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'restaurant_location_id' => 'sometimes|exists:restaurant_locations,id',
+            'restaurant_id'          => 'sometimes|exists:restaurants,id',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'restaurant_location_id.exists' => 'Branch not found.',
+            'restaurant_id.exists'          => 'Restaurant not found.',
+        ];
+    }
+}
