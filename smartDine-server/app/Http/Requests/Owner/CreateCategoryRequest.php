785fc4ab4cd5a810c8bcabef7fe7cc79ab0b381a
Yehia Arja <yehia.al.arja12@gmail.com>
2025-05-03 18:37:04 +0300

    added the creating category request

---
+++ smartDine-server/app/Http/Requests/Owner/CreateCategoryRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Owner;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateCategoryRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'restaurant_id' => 'required|exists:restaurants,id',
+            'name'          => 'required|string|max:255',
+            'file_name'     => 'required|string',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'restaurant_id.required' => 'You must supply a restaurant to attach this category to.',
+            'restaurant_id.exists'   => 'That restaurant does not exist.',
+            'name.required'          => 'Please give the category a name.',
+            'file_name.required'     => 'Please provide a file name.',
+        ];
+    }
+}
