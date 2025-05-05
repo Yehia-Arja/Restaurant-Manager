264815113d1e15cf2d76223d39daac13f0c04049
Yehia Arja <yehia.al.arja12@gmail.com>
2025-05-05 06:59:06 +0300

    seperated the controllers so admin has his own controller

---
+++ smartDine-server/app/Services/Owner/ProductService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Services\Common;
+
+use App\Models\Product;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Auth;
+
+class ProductService
+{
+    /**
+     * Create a new or update product.
+     */
+    public static function upsert(array $data): Product
+    {
+        // Create or update the product
+        return Product::updateOrCreate(
+            ['id' => $data['id'] ?? null],
+            [
+                'restaurant_id'    => $data['restaurant_id'],
+                'name'             => $data['name'],
+                'description'      => $data['description'] ?? null,
+                'file_name'        => $data['file_name'] ?? null,
+                'category_id'      => $data['category_id'] ?? null,
+                'price'            => $data['price'],
+            ]
+        );
+    }
+
+    
+    public static function delete(int $id): bool
+    {
+        // Delete the product by ID
+        return Product::destroy($id) > 0;
+    }
+
+}
