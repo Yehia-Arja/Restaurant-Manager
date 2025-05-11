1499758b7188a4c139967c7d3ac6081d4e767e5c
Yehia Arja <yehia.al.arja12@gmail.com>
2025-05-11 19:39:55 +0300

    added the favorite flag for the restaurants model

--- smartDine-mobile/lib/features/restaurant_selection/data/models/restaurant_model.dart
+++ smartDine-mobile/lib/features/restaurant_selection/data/models/restaurant_model.dart
@@ -1,10 +1,19 @@
+import 'package:flutter/foundation.dart';
+
 class RestaurantModel {
   final int id;
   final String name;
   final String imageUrl;
   final String? description;
+  final bool isFavorite;
 
-  RestaurantModel({required this.id, required this.name, required this.imageUrl, this.description});
+  RestaurantModel({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+    this.description,
+    this.isFavorite = false,
+  });
 
   factory RestaurantModel.fromJson(Map<String, dynamic> json) {
     return RestaurantModel(
@@ -12,6 +21,7 @@ class RestaurantModel {
       name: json['name'] as String,
       imageUrl: json['image_url'] as String,
       description: json['description'] as String?,
+      isFavorite: json['is_favorite'] as bool? ?? false,
     );
   }
 }
