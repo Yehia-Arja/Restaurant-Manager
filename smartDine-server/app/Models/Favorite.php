fc6c8d276cff5546b642c8573a6dbe2479bd4f77
Yehia Arja <yehia.al.arja12@gmail.com>
2025-05-11 22:57:45 +0300

    added the feature of favoriting and unfavoriting a product, restaurant... with the proper routes

--- smartDine-server/app/Models/Favorite.php
+++ smartDine-server/app/Models/Favorite.php
@@ -9,4 +9,15 @@ class Favorite extends Model
 {
     /** @use HasFactory<\Database\Factories\FavoriteFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'favoritable_id',
+        'favoritable_type',
+    ];
+
+    public function favoritable()
+    {
+        return $this->morphTo();
+    }
 }
--- smartDine-server/database/migrations/2025_04_23_150218_create_favorites_table.php
+++ smartDine-server/database/migrations/2025_04_23_150218_create_favorites_table.php
@@ -4,31 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('favoritable_id');
+            $table->string('favoritable_type');
             $table->timestamps();
-            
-            // Foreign keys
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('product_id')->nullable()->constrained('products')->onDelete('cascade');
-            $table->foreignId('restaurant_id')->nullable()->constrained('restaurants')->onDelete('cascade');
 
-            // Uniqueness for product and restaurant favorites
-            $table->unique(['user_id', 'product_id']);
-            $table->unique(['user_id', 'restaurant_id']);
+            // Unique constraint
+            $table->unique(['user_id', 'favoritable_id', 'favoritable_type'], 'user_fav_unique');
+
+            // Foreign key
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('favorites');
