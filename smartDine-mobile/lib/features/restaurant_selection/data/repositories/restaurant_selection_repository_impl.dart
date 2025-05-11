a65bf08cfc4ff52000575e98631d43e3dc4d7207
Yehia Arja <yehia.al.arja12@gmail.com>
2025-05-11 13:57:34 +0300

    added my restaurant fetching remote file

--- smartDine-mobile/lib/features/restaurant_selection/data/repositories/restaurant_selection_repository_impl.dart
+++ smartDine-mobile/lib/features/restaurant_selection/data/repositories/restaurant_selection_repository_impl.dart
@@ -1,7 +1,6 @@
 import 'package:mobile/features/restaurant_selection/domain/repositories/restaurant_selection_repository.dart';
 import 'package:mobile/features/restaurant_selection/domain/entities/restaurant.dart';
 import 'package:mobile/features/restaurant_selection/data/datasources/restaurant_selection_remote.dart';
-import 'package:mobile/features/restaurant_selection/data/models/restaurant_model.dart';
 
 class RestaurantSelectionRepositoryImpl extends RestaurantSelectionRepository {
   final RestaurantSelectionRemote _remote;
@@ -10,7 +9,16 @@ class RestaurantSelectionRepositoryImpl extends RestaurantSelectionRepository {
 
   @override
   Future<List<Restaurant>> getRestaurants() async {
-    final restaurantModels = await _remote.getRestaurants();
-    return restaurantModels.map((model) => model.toEntity()).toList();
+    final restaurantModels = await _remote.getRestaurants('common/restaurants');
+    return restaurantModels
+        .map(
+          (model) => Restaurant(
+            id: model.id,
+            name: model.name,
+            imageUrl: model.imageUrl,
+            description: model.description,
+          ),
+        )
+        .toList();
   }
 }
