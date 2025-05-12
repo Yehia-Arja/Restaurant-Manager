862556b07b0e9e3d366b02607d4c4bda21cd93e5
Yehia Arja <yehia.al.arja12@gmail.com>
2025-05-13 02:50:21 +0300

    modified my orders remote file so it uses the new model

--- smartDine-mobile/lib/features/orders/data/datasource/orders_remote.dart
+++ smartDine-mobile/lib/features/orders/data/datasource/orders_remote.dart
@@ -1,5 +1,5 @@
 import 'package:dio/dio.dart';
-import '../model/order_model.dart';
+import '../models/order_model.dart';
 
 class OrdersRemote {
   final Dio _dio;
@@ -10,22 +10,14 @@ class OrdersRemote {
     required int branchId,
     required int tableNumber,
   }) async {
-    try {
-      final response = await _dio.post(
-        'client/orders',
-        data: {
-          'product_id': productId,
-          'restaurant_location_id': branchId,
-          'table_number': tableNumber,
-        },
-      );
-      final json = response.data['data'] as Map<String, dynamic>;
-      return OrderModel.fromJson(json);
-    } on DioException catch (e) {
-      final message = e.response?.data['message'] as String? ?? e.message;
-      throw Exception('Failed to place order: $message');
-    } catch (e) {
-      throw Exception('Unexpected error placing order: $e');
-    }
+    final response = await _dio.post(
+      'client/orders',
+      data: {
+        'product_id': productId,
+        'restaurant_location_id': branchId,
+        'table_number': tableNumber,
+      },
+    );
+    return OrderModel.fromJson(response.data['data'] as Map<String, dynamic>);
   }
 }
