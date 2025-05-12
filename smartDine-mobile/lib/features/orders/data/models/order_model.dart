272133fe55c3239e5d248e1b2aec7568b090db4b
Yehia Arja <yehia.al.arja12@gmail.com>
2025-05-13 02:49:26 +0300

    added the order model

---
+++ smartDine-mobile/lib/features/orders/data/models/order_model.dart
@@ -0,0 +1,28 @@
+import '../../domain/entities/order_entity.dart';
+import 'package:mobile/features/products/data/models/product_model.dart';
+
+class OrderModel extends OrderEntity {
+  OrderModel({
+    required int id,
+    required ProductModel productModel,
+    required int tableNumber,
+    required String status,
+    required DateTime createdAt,
+  }) : super(
+         id: id,
+         product: productModel.toEntity(),
+         tableNumber: tableNumber,
+         status: status,
+         createdAt: createdAt,
+       );
+
+  factory OrderModel.fromJson(Map<String, dynamic> json) {
+    return OrderModel(
+      id: json['id'] as int,
+      productModel: ProductModel.fromJson(json['product'] as Map<String, dynamic>),
+      tableNumber: json['table_id'] as int,
+      status: json['status'] as String,
+      createdAt: DateTime.parse(json['created_at'] as String),
+    );
+  }
+}
