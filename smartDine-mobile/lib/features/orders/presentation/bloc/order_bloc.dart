92319a27374a3110075408f8c43389b7b6cfa1d0
Yehia Arja <yehia.al.arja12@gmail.com>
2025-05-13 02:55:59 +0300

    added the order bloc file

---
+++ smartDine-mobile/lib/features/orders/presentation/bloc/order_bloc.dart
@@ -0,0 +1,26 @@
+import 'package:bloc/bloc.dart';
+import 'order_event.dart';
+import 'order_state.dart';
+import '../../domain/usecases/place_order_usecase.dart';
+
+class OrderBloc extends Bloc<OrderEvent, OrderState> {
+  final PlaceOrderUseCase _placeOrderUseCase;
+
+  OrderBloc(this._placeOrderUseCase) : super(OrderInitial()) {
+    on<PlaceOrderRequested>(_onPlaceOrderRequested);
+  }
+
+  Future<void> _onPlaceOrderRequested(PlaceOrderRequested event, Emitter<OrderState> emit) async {
+    emit(OrderInProgress());
+    try {
+      final order = await _placeOrderUseCase(
+        productId: event.productId,
+        branchId: event.branchId,
+        tableNumber: event.tableNumber,
+      );
+      emit(OrderSuccess(order));
+    } catch (e) {
+      emit(OrderFailure(e.toString()));
+    }
+  }
+}
