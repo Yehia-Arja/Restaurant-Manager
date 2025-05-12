2ee757a6d7704e18c3f60ef9e290cb96db47c209
Yehia Arja <yehia.al.arja12@gmail.com>
2025-05-12 04:54:25 +0300

    added the branch entity

---
+++ smartDine-mobile/lib/features/home/domain/entities/branch.dart
@@ -0,0 +1,15 @@
+class Branch {
+  final int id;
+  final String locationName;
+  final String address;
+  final String city;
+  final int restaurantId;
+
+  Branch({
+    required this.id,
+    required this.locationName,
+    required this.address,
+    required this.city,
+    required this.restaurantId,
+  });
+}
