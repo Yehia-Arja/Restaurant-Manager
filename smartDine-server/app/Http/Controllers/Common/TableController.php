edfa28263ad32767030db10b4f2be90c8baeaea8
Yehia Arja <yehia.al.arja12@gmail.com>
2025-05-13 14:21:47 +0300

    added the get tables common controller

--- smartDine-server/app/Http/Controllers/Common/TableController.php
+++ smartDine-server/app/Http/Controllers/Common/TableController.php
@@ -1,64 +1,37 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Common;
 
-use App\Models\Table;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Common\TableRequest;
 
 class TableController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request)
+    public function index(TableRequest $request)
     {
+        try {
+            $data = $request->validated();
+
+            $tables = TableService::list(
+                $data['branch_id'],
+            );
+
+            if ($tables->isEmpty()) {
+                return $this->error('No tables found', 404);
+            }
+
+            return $this->success(
+                'Tables fetched',
+                TableResource::collection($tables)
+            );
+
+        } catch (\Throwable $e) {
+            return $this->error($e->getMessage(), 500);
+        }
+
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Table $table)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Table $table)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Table $table)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Table $table)
-    {
-        //
-    }
 }
