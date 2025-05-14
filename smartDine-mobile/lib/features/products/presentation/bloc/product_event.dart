665731cbde7fdd9cd7afa307e70eaec5dd929e4a
Yehia Arja <yehia.al.arja12@gmail.com>
2025-05-14 08:37:54 +0300

    modified my product event bloc file so it includes the new pagination and query params

--- smartDine-mobile/lib/features/products/presentation/bloc/product_event.dart
+++ smartDine-mobile/lib/features/products/presentation/bloc/product_event.dart
@@ -11,9 +11,17 @@ class LoadProducts extends ProductEvent {
   final int branchId;
   final String? searchQuery;
   final int? categoryId;
+  final bool favoritesOnly;
+  final int page;
 
-  const LoadProducts({required this.branchId, this.searchQuery, this.categoryId});
+  const LoadProducts({
+    required this.branchId,
+    this.searchQuery,
+    this.categoryId,
+    this.favoritesOnly = false,
+    this.page = 1,
+  });
 
   @override
-  List<Object?> get props => [branchId, searchQuery, categoryId];
+  List<Object?> get props => [branchId, searchQuery, categoryId, favoritesOnly, page];
 }
