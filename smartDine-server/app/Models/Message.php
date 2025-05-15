8dbf131cb13b00e52e72d6b1f48f84ce567dbf00
Yehia Arja <yehia.al.arja12@gmail.com>
2025-05-15 06:58:29 +0300

    added the necessary code in my message  model

--- smartDine-server/app/Models/Message.php
+++ smartDine-server/app/Models/Message.php
@@ -9,4 +9,17 @@ class Message extends Model
 {
     /** @use HasFactory<\Database\Factories\MessageFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'chat_id',
+        'sender_type',
+        'content',
+    ];
+
+
+    public function chat()
+    {
+        return $this->belongsTo(Chat::class);
+    }
 }
