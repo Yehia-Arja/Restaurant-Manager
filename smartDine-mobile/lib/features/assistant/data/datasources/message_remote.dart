68a0dbdf4a19dc4927e1489c297bc93cf8590940
Yehia Arja <yehia.al.arja12@gmail.com>
2025-05-18 01:31:52 +0300

    added the message remote file

--- smartDine-mobile/lib/features/assistant/data/datasources/message_remote.dart
+++ smartDine-mobile/lib/features/assistant/data/datasources/message_remote.dart
@@ -1,1 +1,33 @@
-class 
+import 'package:dio/dio.dart';
+import '../models/message_model.dart';
+
+class MessageRemote {
+  final Dio dio;
+
+  MessageRemote(this.dio);
+
+  Future<MessageModel> sendMessage({
+    required int restaurantLocationId,
+    required String message,
+  }) async {
+    try {
+      final response = await dio.post(
+        'common/chat',
+        data: {'restaurant_location_id': restaurantLocationId, 'message': message},
+      );
+
+      final data = response.data['data'];
+      return MessageModel.fromJson(data);
+    } catch (e) {
+      throw Exception('Failed to send message: $e');
+    }
+  }
+
+  Future<void> deleteMessage(int messageId) async {
+    try {
+      await dio.delete('common/chat/$messageId');
+    } catch (e) {
+      throw Exception('Failed to delete message: $e');
+    }
+  }
+}
