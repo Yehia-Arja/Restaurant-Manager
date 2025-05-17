8e21634711c9c941a167bd46da44e65a06d3392c
Yehia Arja <yehia.al.arja12@gmail.com>
2025-05-17 21:59:46 +0300

    fixed the ci cd

--- smartDine-server/shellScript.sh
+++ smartDine-server/shellScript.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
 
-cp .env.example .env
-php artisan key:generate
+php artisan key:generate --force
+
+
 php artisan config:clear
+php artisan config:cache
+
+# Migrate
 php artisan migrate --force
 
+# Start Laravel
 php artisan serve --host=0.0.0.0 --port=8000
