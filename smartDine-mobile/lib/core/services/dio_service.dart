859455425bbaa627ffeb168130002288a47f50c6
Yehia Arja <yehia.al.arja12@gmail.com>
2025-05-20 01:06:37 +0300

    merged

--- smartDine-mobile/lib/core/services/dio_service.dart
+++ smartDine-mobile/lib/core/services/dio_service.dart
@@ -4,11 +4,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 class DioService {
   static final Dio dio = Dio(
       BaseOptions(
-<<<<<<< HEAD
-        baseUrl: 'http://127.0.0.1/api/v0.1/',
-=======
         baseUrl: 'http://10.0.2.2/api/v0.1/',
->>>>>>> 46d00bb9747338c9e296f5cdfff9328f040e6521
         connectTimeout: const Duration(seconds: 100),
         receiveTimeout: const Duration(seconds: 100),
         headers: {'Accept': 'application/json'},
